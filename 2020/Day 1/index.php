5042c8688eec0eaddb6ba275049861069bed15f0
Jorge González <yo@jorgeglz.io>
2020-12-02 13:18:30 -0600

    Day 2, with some improvements for Day 1

--- 2020/Day 1/index.php
+++ 2020/Day 1/index.php
@@ -1,11 +1,8 @@
 <?php
 
-$puzzle = array_filter(
-    explode(
-        PHP_EOL,
-        file_get_contents(__DIR__.'/Puzzle.txt')
-    )
-);
+include_once __DIR__.'/../vendor/autoload.php';
+
+$puzzle = file(__DIR__.'/puzzle.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 // Part 1: Find 2 pieces that summed up result in '2020'. Store their product.
 $two_part_result = null;
@@ -43,7 +40,6 @@ foreach ($puzzle as $first_piece) {
     }
 }
 
-echo "Your 2-part expense report result is '{$two_part_result}'." . PHP_EOL;
-
-echo "Your 3-part expense report result is '{$three_part_result}'." . PHP_EOL;
+echo "[Part 1] Your 2-part expense report result is '{$two_part_result}'." . PHP_EOL;
+echo "[Part 2] Your 3-part expense report result is '{$three_part_result}'." . PHP_EOL;
 
---
+++ 2020/Day 2/index.php
@@ -0,0 +1,63 @@
+<?php
+
+include_once __DIR__.'/../vendor/autoload.php';
+
+$puzzle = file(__DIR__.'/puzzle.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$valid_passwords_from_character_count = 0;
+$valid_passwords_from_character_position = 0;
+
+function character_count_restriction(
+    string $password,
+    string $character_restriction,
+    int $min_count_restriction,
+    int $max_count_restriction
+): bool
+{
+    $password_character_count = array_count_values(str_split($password));
+    $restricted_character_count = $password_character_count[$character_restriction] ?? 0;
+
+    return $restricted_character_count >= $min_count_restriction
+        && $restricted_character_count <= $max_count_restriction;
+}
+
+function character_position_restriction(
+    string $password,
+    string $character_restriction,
+    int $first_position,
+    int $second_position
+): bool
+{
+    // Note: Remember they don't have a Zero-index policy, but PHP does :sweat_smile:
+    --$first_position;
+    --$second_position;
+
+    return $character_restriction === $password[$first_position]
+        xor $character_restriction === $password[$second_position];
+}
+
+foreach ($puzzle as $line) {
+    [$restriction_count, $restriction_letter, $password] = explode(' ', $line);
+
+    [$restriction_min, $restriction_max] = explode('-', $restriction_count);
+    $restriction_letter = str_replace(':', '', $restriction_letter);
+
+    // Part 1
+    if (character_count_restriction(
+        $password,
+        $restriction_letter,
+        (int) $restriction_min,
+        (int) $restriction_max
+    )) ++$valid_passwords_from_character_count;
+
+    // Part 2
+    if (character_position_restriction(
+        $password,
+        $restriction_letter,
+        (int) $restriction_min,
+        (int) $restriction_max
+    )) ++$valid_passwords_from_character_position;
+}
+
+echo "[Part 1] There are {$valid_passwords_from_character_count} valid passwords in the database." . PHP_EOL;
+echo "[Part 2] There are {$valid_passwords_from_character_position} valid passwords in the database." . PHP_EOL;
